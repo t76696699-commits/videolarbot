edfdd82ed60917cf3a3dc7ebbd4f5fcc75004f4f
t76696699-commits <t76696699@gmail.com>
2026-07-22 11:36:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,23 @@ TOKEN = "8869424579:AAGblPU6D0i7tu08RgE9FW3KqRZ2VUsmvdU"
 CHANNEL_ID = -1004354334641
 
 
-bot = Bot(token=TOKEN)
+ot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Har bir "Kod" va kanalning real xabar ID raqamini shu yerga to'g'ridan-to'g'ri yozib chiqamiz
+VIDEO_MAPPING = {
+    1: 9,   # Kod: 1 uchun kanalning xabar ID raqami
+    4: 12,  # Kod: 4 uchun kanalning xabar ID raqami
+    5: 13,
+    6: 14,
+    7: 15,
+    8: 16,
+    10: 18,
+    14: 22,
+    15: 23,
+    # Qolgan kodlarni ham shunday ko'rinishda davom ettirib yozib qo'yasiz:
+    # Kod : Telegram_Xabar_ID
+}
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
@@ -19,12 +33,17 @@ async def cmd_start(message: types.Message):
         "men kanaldan o'sha videoni topib yuboraman."
     )
 
-
 @dp.message(F.text)
 async def send_video_from_channel(message: types.Message):
     if message.text.isdigit():
-        # Farq 8 taga teng bo'lgani uchun - 8 yozamiz
-        video_message_id = int(message.text) - 8
+        code = int(message.text)
+        
+        # Agar kiritilgan kod lug'atda bo'lsa, uning real ID raqamini olamiz
+        if code in VIDEO_MAPPING:
+            video_message_id = VIDEO_MAPPING[code]
+        else:
+            await message.answer("❌ Bunday raqamli video topilmadi.")
+            return
         
         try:
             await bot.copy_message(
@@ -33,12 +52,10 @@ async def send_video_from_channel(message: types.Message):
                 message_id=video_message_id
             )
         except Exception as e:
-            await message.answer("❌ Bunday raqamli video topilmadi yoki xatolik yuz berdi.")
-
+            await message.answer("❌ Video topilmadi yoki xatolik yuz berdi.")
 
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
